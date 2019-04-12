aa14177347ca4a0a7d58b84bb2b50746eeb734fc
Michael C Li <michael.li@medium.one>
2019-04-11 17:54:48 -0700

    add page program

--- src/main_thread_entry.cpp
+++ src/main_thread_entry.cpp
@@ -65,13 +65,20 @@ SPI1    SPI = SPI1();   // SPI 1 port
 #define  ARRAY_READ_CMD                 0x03  // 1+ bytes
 #define  ARRAY_FASTREAD_CMD             0x0B  // 1+ bytes
 
+#define  PAGE_PROGRAM_CMD               0x02
+
+#define READHEADER       3   // 3 address bytes
+#define FASTREADHEADER   4   // 3 address bytes + 1 dummy bytes
+#define PGMHEADER        4   // 1 command + 3 address bytes (page program)
+
 void display_status_register(char statusbyte);
 void display_flag_status_register(char statusbyte);
 void display_ID_data(char *data);
+void read_array_data(char *data, uint8_t sect_add, uint8_t pgm_add, uint8_t byte_add, uint16_t len);
 
 void setup() {
     char data[256], buf[40];
-    char cmd[10];   // only the first byte is used.
+    char cmd[256+8];   // only the first byte is used.
 
     Serial.begin(9600);
     //while(!Serial);
@@ -147,6 +154,7 @@ void setup() {
     Serial.println(" HEX");
     display_flag_status_register(data[0]);
 
+    /*
     Serial.println("==============================");
     Serial.println("Send CLEAR_FLAG_STATUS command.");
     cmd[0] = CLEAR_FLAG_STATUS_REGISTER_CMD;
@@ -167,31 +175,110 @@ void setup() {
     Serial.print(data[0] & 0x00FF,HEX);
     Serial.println(" HEX");
     display_flag_status_register(data[0]);
-
+    */
     Serial.println("==============================");
-#define READHEADER   3   // 3 address bytes
+
+
+    cmd[0] = PAGE_PROGRAM_CMD;
+    cmd[1] = 0x00;  //A<24:16> - Erase Sectors
+                    //A<15:12> - Erase Subsectors
+    cmd[2] = 0x06;  //A<11:8> -  Program Pages
+    cmd[3] = 0x00;  //A<7:0> bytes in Page address
+
+    for (int i=0; i<256; i++)
+        cmd[i+4] = i + 0x70;
+    Serial.println("Page Program");
+    SPI.write_transfer(cmd, PGMHEADER+256);
+
+    char status;
+    int  counter = 0;
+    do {
+        cmd[0] = READ_STATUS_REG_CMD;
+        SPI.readwrite_transfer(cmd, data, 1);
+        Serial.println(data[0] & 0x00FF,HEX);
+        status = data[0];
+    } while ((status & 0x01) && (counter++ < 10));
+
+
+     Serial.println("==============================");
+     cmd[0] = READ_STATUS_REG_CMD;
+     SPI.readwrite_transfer(cmd, data, 1);
+     Serial.print("Status Register's Value = ");
+     Serial.println(data[0] & 0x00FF,HEX);
+     Serial.println(" HEX");
+     display_status_register(data[0]);
+     status = data[0];
+
+
+     Serial.println("==============================");
+     cmd[0] = READ_FLAG_STATUS_REGISTER_CMD;
+     SPI.readwrite_transfer(cmd, data, 1);
+     Serial.print("Flag Status Register's Value = ");
+     Serial.print(data[0] & 0x00FF,HEX);
+     Serial.println(" HEX");
+     display_flag_status_register(data[0]);
+
+     read_array_data(data, 0, 6, 0, 256);
+
+     for (int i=0; i< 256; i++) {
+         if ((data[READHEADER+i] & 0x00FF) < 0x0010) Serial.print(" ");
+         Serial.print(data[READHEADER+i] & 0x00FF,HEX);
+         Serial.print(", ");
+         if (i%10 == 9) Serial.println(" ");
+         if (data[READHEADER+i] != (char)(0x70+i))
+             Serial.print(" *mismatch* ");
+     }
+     Serial.println(" ");
+
+    int  sect_add = 0;
+    int  pgm_add = 0;
+    int  byte_add = 0;
+    int  datalen = 1;
+
+    for (pgm_add = 0; pgm_add < 16; pgm_add++) {
+        read_array_data(data, sect_add, pgm_add, byte_add, datalen);
+        Serial.print("Array Data Value (READ) = ");
+        Serial.print(data[READHEADER] & 0x00FF,HEX);
+        Serial.println(" HEX");
+    }
+
+    /*
+    Serial.println("==============================");
+
     cmd[0] = ARRAY_READ_CMD;
-    cmd[1] = 0x00;  //A<24:0>
-    cmd[2] = 0x00;
-    cmd[3] = 0x01;
+    cmd[1] = 0x00;  //A<24:16> - Erase Sectors
+                    //A<15:12> - Erase Subsectors
+    cmd[2] = 0x00;  //A<11:8> -  Program Pages
+    cmd[3] = 0x00;  //A<7:0> bytes in Page address
     cmd[4] = 0x85; //DATA IN
-    SPI.readwrite_transfer(cmd, data, READHEADER + 1);  //  1 data byte
+    SPI.readwrite_transfer(cmd, data, READHEADER + datalen);  //  1 data byte
     Serial.print("Array Data Value (READ) = ");
     Serial.print(data[READHEADER] & 0x00FF,HEX);
     Serial.println(" HEX");
 
+    for (int i=0; i<datalen; i++) {
+        Serial.print(data[READHEADER+i] & 0x00FF,HEX);
+        Serial.println(" HEX");
+    }
+
 
     Serial.println("==============================");
-#define FASTREADHEADER   4   // 3 address bytes + 1 dummy bytes
     cmd[0] = ARRAY_FASTREAD_CMD;
     cmd[1] = 0x00;  //A<24:0>
     cmd[2] = 0x00;
     cmd[3] = 0x01;
     cmd[4] = 0x85; //DATA IN
-    SPI.readwrite_transfer(cmd, data, FASTREADHEADER + 1);  //  1 data byte
+    SPI.readwrite_transfer(cmd, data, FASTREADHEADER + datalen);  //  1 data byte
     Serial.print("Array Data Value (FAST READ) = ");
     Serial.print(data[FASTREADHEADER] & 0x00FF,HEX);
     Serial.println(" HEX");
+
+    for (int i=0; i<datalen; i++) {
+        Serial.print(data[READHEADER+i] & 0x00FF,HEX);
+        Serial.println(" HEX");
+    }
+
+    */
 }
 
 void loop() {
@@ -282,6 +369,145 @@ void display_ID_data(char *data) {
      Serial.println(" HEX");
 
 }
+
+
+
+
+// Read array bytes for general cases.
+// input:  erase sector address (8 bits), program page address (8 bits), byte address (8 bits), len (255 max)
+// output: data (array size required = len + READHEADER + 1)
+void read_array_data(char *data, uint8_t sect_add, uint8_t pgm_add, uint8_t byte_add, uint16_t len) {
+    char cmd[10];
+    Serial.println("==============================");
+
+    cmd[0] = ARRAY_READ_CMD;
+    cmd[1] = sect_add;   //A<24:16> - Erase Sectors
+                         //A<15:12> - Erase Subsectors
+    cmd[2] = pgm_add;    //A<11:8> -  Program Pages
+    cmd[3] = byte_add;   //A<7:0> bytes in Page address
+    cmd[4] = 0x85; //First DATA IN
+
+    Serial.print(cmd[0] & 0x00FF,HEX);
+    Serial.print(", ");
+    Serial.print(cmd[1] & 0x00FF,HEX);
+    Serial.print(", ");
+    Serial.print(cmd[2] & 0x00FF,HEX);
+    Serial.print(", ");
+    Serial.print(cmd[3] & 0x00FF,HEX);
+    Serial.println(" ");
+
+    if (len <= 256)
+        SPI.readwrite_transfer(cmd, data, READHEADER + len);
+}
+
+// Read array bytes for a specified program page.
+// input:  program page address (16 bits), byte address (8 bits), len (255 max)
+// output: data (array size required = len + READHEADER + 1)
+void read_array_data_pgmaddr(char *data, uint16_t pgm_add, uint8_t byte_add, uint16_t len) {
+    char cmd[10];
+    Serial.println("==============================");
+
+    cmd[0] = ARRAY_READ_CMD;
+    cmd[1] = (pgm_add >> 8) & 0x00FF;   //A<24:16> - Erase Sectors
+                                        //A<15:12> - Erase Subsectors
+    cmd[2] = pgm_add & 0x00FF;          //A<11:8> -  Program Pages
+    cmd[3] = byte_add;                  //A<7:0> bytes in Page address
+    cmd[4] = 0x85; //First DATA IN
+
+    Serial.print(cmd[0] & 0x00FF,HEX);
+    Serial.print(", ");
+    Serial.print(cmd[1] & 0x00FF,HEX);
+    Serial.print(", ");
+    Serial.print(cmd[2] & 0x00FF,HEX);
+    Serial.print(", ");
+    Serial.print(cmd[3] & 0x00FF,HEX);
+    Serial.println(" ");
+
+    if (len <= 256)
+        SPI.readwrite_transfer(cmd, data, READHEADER + len);
+}
+
+// Read array bytes for a specified sub sector.
+// input:  subsector address (12 bits), byte address (12 bits), len (255 max)
+// output: data (array size required = len + READHEADER + 1)
+void read_array_data_subsectaddr(char *data, uint16_t subsect_add, uint16_t byte_add, int len) {
+    char cmd[10];
+    Serial.println("==============================");
+
+    cmd[0] = ARRAY_READ_CMD;
+    cmd[1] = (subsect_add >> 4) & 0x00FF;   //A<24:16> - Erase Sectors
+                                            //A<15:12> - Erase Subsectors
+    cmd[2] = ((subsect_add << 4) & 0x00F0) | ((byte_add >> 8) & 0x000F);   //A<11:8> -  Program Pages
+    cmd[3] = byte_add & 0x00FF;                                  //A<7:0> bytes in Page address
+    cmd[4] = 0x85; //First DATA IN
+
+    Serial.print(cmd[0] & 0x00FF,HEX);
+    Serial.print(", ");
+    Serial.print(cmd[1] & 0x00FF,HEX);
+    Serial.print(", ");
+    Serial.print(cmd[2] & 0x00FF,HEX);
+    Serial.print(", ");
+    Serial.print(cmd[3] & 0x00FF,HEX);
+    Serial.println(" ");
+
+    if (len <= 256)
+        SPI.readwrite_transfer(cmd, data, READHEADER + len);
+}
+
+// Read array bytes for a specified sub sector.
+// input:  sector address (8 bits), byte address (16 bits), len (255 max)
+// output: data (array size required = len + READHEADER + 1)
+void read_array_data_sectaddr(char *data, uint8_t sect_add, uint16_t byte_add, int len) {
+    char cmd[10];
+    Serial.println("==============================");
+
+    cmd[0] = ARRAY_READ_CMD;
+    cmd[1] = sect_add;      //A<24:16> - Erase Sectors
+                            //A<15:12> - Erase Subsectors
+    cmd[2] = (byte_add >> 8) & 0x00FF;   //A<11:8> -  Program Pages
+    cmd[3] = byte_add & 0x00FF;          //A<7:0> bytes in Page address
+    cmd[4] = 0x85; //First DATA IN
+
+    Serial.print(cmd[0] & 0x00FF,HEX);
+    Serial.print(", ");
+    Serial.print(cmd[1] & 0x00FF,HEX);
+    Serial.print(", ");
+    Serial.print(cmd[2] & 0x00FF,HEX);
+    Serial.print(", ");
+    Serial.print(cmd[3] & 0x00FF,HEX);
+    Serial.println(" ");
+
+    if (len <= 256)
+        SPI.readwrite_transfer(cmd, data, READHEADER + len);
+}
+
+// Fast Read array bytes for general cases.
+// input:  erase sector address (8 bits), program page address (8 bits), byte address (8 bits), len (255 max)
+// output: data (array size required = len + READHEADER + 1)
+void fast_read_array_data(char *data, uint8_t sect_add, uint8_t pgm_add, uint8_t byte_add, uint16_t len) {
+    char cmd[10];
+    Serial.println("==============================");
+
+    cmd[0] = ARRAY_FASTREAD_CMD;
+    cmd[1] = sect_add;   //A<24:16> - Erase Sectors
+                         //A<15:12> - Erase Subsectors
+    cmd[2] = pgm_add;    //A<11:8> -  Program Pages
+    cmd[3] = byte_add;   //A<7:0> bytes in Page address
+    cmd[4] = 0x85; //First DATA IN
+
+    Serial.print(cmd[0] & 0x00FF,HEX);
+    Serial.print(", ");
+    Serial.print(cmd[1] & 0x00FF,HEX);
+    Serial.print(", ");
+    Serial.print(cmd[2] & 0x00FF,HEX);
+    Serial.print(", ");
+    Serial.print(cmd[3] & 0x00FF,HEX);
+    Serial.println(" ");
+
+    if (len <= 256)
+        SPI.readwrite_transfer(cmd, data, FASTREADHEADER + len);
+}
+
 //====================== Your Arduino Example Sketch End ===========//
 
 
